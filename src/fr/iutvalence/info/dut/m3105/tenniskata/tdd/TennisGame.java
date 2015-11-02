938c7e44759ee5f4626c3617cb8254cfad872a40
mounieju <mounieju@PC-B103-01.iut-valence.fr>
2015-11-02 17:48:16 +0100

    Iteration 2 : Green implement method receiverScores() in TennisGame Class and new attribute receiverScore

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -4,22 +4,34 @@ public class TennisGame
 	public static final String LOVE_ALL = "love-all";
 	public static final String FIFTEEN_LOVE = "fifteen-love";
 	private int serverScore;
+	private int receiverScore;
 
 	public String getScore() 
 	{
-		if (this.serverScore==15)
+		if (this.serverScore==15 && this.receiverScore==0)
 		{
 			return FIFTEEN_LOVE;
+		}
+		if (this.receiverScore==15 && this.serverScore==0)
+		{
+			return "love-fifteen";
 		}
 		else
 		{
 			return LOVE_ALL;
 		}
+		
 	}
 
 	public void serverScores() 
 	{	
 		this.serverScore += 15;
+	}
+
+	public void receiverScores() 
+	{
+		this.receiverScore += 15;
+		
 	}
 
 
